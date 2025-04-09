1f9f335599a49d55184149d473ffc27cebdc013b
lucastavares06 <lucastavareswin10@outlook.com>
2025-04-08 21:54:40 -0300

    refactor(menu): extract menu display logic into its own module

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -1,36 +1,12 @@
 #include "menu.h"
 #include "../utils/utils.h"
-#include "../character/character.h"
 
-int run_game_menu(void) {
-    char option;
-    Character main_character;
-    do {
-        println("");
-        println("Main Menu:");
-        println("  1. Create Character");
-        println("  2. Start Battle");
-        println("  3. Exit");
-        print("Enter your option: ");
-        option = read_digit_char();
-        switch (option) {
-            case '1':
-                println("Creating character...");
-            if (create_character(&main_character) == 0)
-                println("Character created successfully!");
-            else
-                println("Error creating character.");
-            break;
-            case '2':
-                println("Starting battle (under construction)...");
-            break;
-            case '3':
-                println("Exiting game...");
-            break;
-            default:
-                println("Invalid option. Try again.");
-            break;
-        }
-    } while (option != '3');
-    return 0;
+char display_main_menu(void) {
+    println("");
+    println("Main Menu:");
+    println("  1. Create Character");
+    println("  2. Start Battle");
+    println("  3. Exit");
+    print("Enter your option: ");
+    return read_digit_char();
 }
---
+++ src/menu/menu.h
@@ -0,0 +1,6 @@
+#ifndef MENU_H
+#define MENU_H
+
+char display_main_menu(void);
+
+#endif
