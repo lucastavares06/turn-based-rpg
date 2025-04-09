18bad3ead8e16082a05aff25c8e6a653f40795b7
lucastavares06 <lucastavareswin10@outlook.com>
2025-04-08 21:59:02 -0300

    feat(game): add game_run function to handle main game loop

---
+++ src/game/game.c
@@ -0,0 +1,33 @@
+#include "game.h"
+#include "../menu/menu.h"
+#include "../character/character.h"
+#include "../utils/utils.h"
+
+int game_run(void) {
+    char option;
+    Character main_character;
+    int result;
+    do {
+        option = display_main_menu();
+        switch (option) {
+            case '1':
+                println("Creating character...");
+            result = create_character(&main_character);
+            if (result == 0)
+                println("Character created successfully!");
+            else
+                println("Error creating character.");
+            break;
+            case '2':
+                println("Starting battle (under construction)...");
+            break;
+            case '3':
+                println("Exiting game...");
+            break;
+            default:
+                println("Invalid option. Try again.");
+            break;
+        }
+    } while (option != '3');
+    return 0;
+}
