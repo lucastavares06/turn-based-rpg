f3466ecc0d243862ffa1bcc938da2a013a9e341f
lucastavares06 <lucastavareswin10@outlook.com>
2025-04-10 21:54:36 -0300

    feat(utils): add colors.h for terminal color definitions

---
+++ src/utils/colors.h
@@ -0,0 +1,11 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define ANSI_COLOR_RED              "\033[0;31m"
+#define ANSI_COLOR_YELLOW           "\033[0;33m"
+#define ANSI_COLOR_GREEN            "\033[0;32m"
+#define ANSI_COLOR_ORANGE           "\x1b[38;5;208m"
+#define ANSI_COLOR_RESET            "\033[0m"
+#define ANSI_COLOR_BRIGHT_YELLOW    "\x1b[93m"
+
+#endif
